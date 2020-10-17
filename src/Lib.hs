c08a7802cae3d0a8b5708f852b1dbf818923356d
Alexander Dolgikh <alexander.dolgikh@corp.mail.ru>
2020-10-18 02:15:48 +0300

    check without transposing refactoring play again after finish

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,10 +11,7 @@ module Lib
     ) where
 
 import Data.Matrix hiding (trace)
-import Data.List (nub, sortOn)
-import Debug.Trace
-import qualified Data.HashMap.Strict as H
-import Control.Monad.Par
+import Data.List (sortOn)
 
 data XO = X | O | Null deriving (Show, Eq, Read)
 
@@ -56,20 +53,21 @@ createBlankMatrix n = matrix n n (\(_,_) -> Null)
 
 checkWin :: Matrix XO -> XO
 checkWin matrix =
-    case nub $  filter (\case Null -> False; _ -> True) xoList of
+    case filter (\case Null -> False; _ -> True) xoList of
         []   -> Null
         [xo] -> xo
         _    -> error "We can not have more than one win"
 
     where
-        xoList = [checkRowsAndDiagonal xo n m | xo <- [X, O], m <- [matrix, transpose matrix]]
+        xoList = [checkRowsAndDiagonal xo n matrix | xo <- [X, O]]
         n = nrows matrix
         checkRowsAndDiagonal xo n matrix = let
-                rowsWin = any id [all (==xo) (getRow i matrix)| i <- [1..n]]
-                diagWin = all (==xo) $ getDiag matrix
+                rowsWin = or [all (==xo) (getRow i matrix)| i <- [1..n]]
+                colsWin = or [all (==xo) (getCol i matrix)| i <- [1..n]]
+                diagWin = all (==xo) $ map (\i -> getElem i i matrix) $ [1..n]
                 anotherDiagWin = all (==xo) $ map (\(i, j) -> getElem i j matrix) $ zip [1..n] [n, n-1 .. 1]
             in
-                if rowsWin || diagWin || anotherDiagWin
+                if rowsWin || diagWin || anotherDiagWin || colsWin
                 then xo
                 else Null
 
@@ -89,10 +87,14 @@ findNulls matrix = [(i, j) | i <- [1..n], j <- [1..n], getElem i j matrix == Nul
 
 checkGameState :: Matrix XO -> XO -> GameState
 checkGameState matrix xo
-    | checkWin matrix == Null && checkFull matrix = Draw
-    | checkWin matrix == xo                       = Win
-    | checkWin matrix == swapXO xo                = Losing
-    | otherwise                                   = Continue
+    | winXO == Null && checkFull matrix = Draw
+    | winXO == xo                       = Win
+    | winXO == swapXO xo                = Losing
+    | otherwise                         = Continue
+      where
+          winXO = checkWin matrix
+
+-- checkGameStateM = memoize2 checkGameState
 
 xoToStr :: XO -> [Char]
 xoToStr X = "2"
@@ -109,71 +111,98 @@ hashFunction xo matrix maximize = read bigString :: Int
         matrixList = concatMap xoToStr $ toList matrix
         bigString = xoToStr xo ++ maximizeToStr maximize ++ matrixList
 
-makeBestDecision :: XO -> [Matrix XO] -> Bool -> Game
-makeBestDecision xo matrix@(m:ms) maximize = case checkGameState m (if maximize then xo else swapXO xo) of
-        Draw -> Game Draw matrix
-        Win -> Game Win matrix
-        Losing -> Game Losing matrix
-        Continue -> let
-                freePlaces = findNulls m
-                newMatrixList = map (\coords -> setElem xo coords m) freePlaces
-                notXO = swapXO xo
-                decisions = map (\newM -> makeBestDecision notXO (newM : matrix) (not maximize)) newMatrixList
-                sorted = if maximize
-                    then reverse $ sortOn state decisions
-                    else sortOn state decisions
-                getResult lst = head $ sortOn (length . returnMatrix) $ filterSame lst
-            in
-                case sorted of
-                    [] -> error "We can not have blank list"
-                    lst -> getResult lst
-
-
-filterSame :: [Game] -> [Game]
-filterSame lst@(Game Win _ : _) = filter (\case Game Win _ -> True; Game _ _ -> False) lst
-filterSame lst@(Game Draw _ : _) = filter (\case Game Draw _ -> True; Game _ _ -> False) lst
-filterSame lst@(Game Continue _ : _) = filter (\case Game Continue _ -> True; Game _ _ -> False) lst
-filterSame lst@(Game Losing _ : _) = filter (\case Game Losing _ -> True; Game _ _ -> False) lst
-
-
-nextStepMatrix :: XO -> Matrix XO -> Matrix XO
-nextStepMatrix xo matrix = newMatrix
-    where
-        matrixList = returnMatrix $ makeBestDecision xo [matrix] True
-        newMatrix = head $ tail $ reverse matrixList
 
 matrixStart :: Matrix XO
 matrixStart = fromLists [[Null, Null, O], [Null, X, X], [Null, Null, O]]
 
+
 interactiveGame :: IO ()
-interactiveGame = do
-    print "Choose X or O"
-    xoStr <- getLine
-    let xo = case read xoStr :: XO of
-               Null -> error "It can not be NULL"
-               xo   -> xo
-    print "Choose 1 or 2"
-    positionStr <- getLine
-    let position = read positionStr :: Int
-    print "Choose size of game field"
-    nStr <- getLine
-    let n = read nStr :: Int
-    let xoStart = [xo, swapXO xo] !! (position - 1)
-    let startMatrix = createBlankMatrix n
-    result <- gameProcess xoStart xo startMatrix
-    print result
+interactiveGame = gameProcessCycle
         where
-            gameProcess :: XO -> XO -> Matrix XO -> IO [Char]
+            -- gameProcess :: XO -> XO -> Matrix XO -> IO [Char]
             gameProcess xo humanXO matrix = case checkGameState matrix xo of
                 Win -> return $ show xo ++ " win!!!"
                 Draw -> return $ "Draw("
                 Losing -> return $ show xo ++ " lose((("
                 Continue -> if xo == humanXO
                     then (\x -> gameProcess (swapXO xo) humanXO (setElem humanXO x matrix)) =<< getCoordinates matrix
-                    else gameProcess (swapXO xo) humanXO (nextStepMatrix xo matrix)
+                    else gameProcess (swapXO xo) humanXO newMatrix
+                        where
+                            newMatrix = nextStepMatrix xo matrix
             getCoordinates matrix = do
                 print matrix
                 print "Your turn, input x and y"
                 xyStr <- getLine
                 let xyList = map (\x -> read x :: Int) $ take 2 $ words xyStr
                 return (head xyList, head $ tail xyList)
+
+            gameProcessCycle = do
+                print "Choose X or O"
+                xoStr <- getLine
+                let xo = case read xoStr :: XO of
+                          Null -> error "It can not be NULL"
+                          xo   -> xo
+                print "Choose 1 or 2"
+                positionStr <- getLine
+                let position = read positionStr :: Int
+                print "Choose size of game field"
+                nStr <- getLine
+                let n = read nStr :: Int
+                let xoStart = [xo, swapXO xo] !! (position - 1)
+                let startMatrix = createBlankMatrix n
+                result <- gameProcess xoStart xo startMatrix
+                print "Result : "
+                print result
+                print "Start new game? Y/n"
+                continue <- getLine
+                if continue == "Y"
+                    then gameProcessCycle
+                    else return ()
+
+
+
+nextStepMatrix :: XO -> Matrix XO -> Matrix XO
+nextStepMatrix xo matrix = newMatrix
+    where
+      newMatrix = setElem xo (makeBestDecision xo matrix True) $ matrix
+
+makeBestDecision :: XO -> Matrix XO -> Bool -> (Int, Int)
+makeBestDecision xo matrix maximize = let
+        freePlaces = findNulls matrix
+        checkPos xo coord matrix minMax =
+            let
+                newMatrix = setElem xo coord matrix
+                minMaxXO = if not minMax then xo else swapXO xo
+                checkPos' = map (\xy -> checkPos (swapXO xo) xy newMatrix (not minMax)) $ findNulls newMatrix
+            in
+                case checkGameState newMatrix minMaxXO of
+                    Draw -> 0
+                    Win -> 10
+                    Losing -> (-10)
+                    Continue -> if minMax
+                        then maximum checkPos'
+                        else minimum checkPos'
+    in
+        snd $ last $ sortOn fst $ map (\crd -> ((checkPos xo crd matrix (not maximize)), crd)) $ freePlaces
+
+  -- case checkGameState matrix (if maximize then xo else swapXO xo) of
+  --       Draw -> (Game Draw matrix, cash)
+  --       Win -> (Game Win matrix, cash)
+  --       Losing -> (Game Losing matrix, cash)
+  --       Continue -> let
+  --               freePlaces = findNulls m
+  --               newMatrixList = map (\coords -> setElem xo coords m) freePlaces
+  --               notXO = swapXO xo
+  --               (decisions, cashes) = unzip $ map (\newM -> makeBestDecision notXO (newM : matrix) (not maximize) cash) newMatrixList
+  --               newCash = foldl1 H.union cashes
+  --               sorted = if maximize
+  --                           then reverse $ sortOn state decisions
+  --                           else sortOn state decisions
+  --               getResult lst = head $ sortOn (length . returnMatrix) $ filterSame lst
+  --               hash = hashFunction xo m maximize
+  --           in
+  --               if hash `H.member` cash
+  --                   then (fromJust $ H.lookup hash cash, cash)
+  --                   else case sorted of
+  --                             [] -> error "We can not have blank list"
+  --                             lst -> (getResult lst, newCash)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,89 +1,101 @@
-import Test.HUnit
-
-import Lib
 import Data.Matrix
+import Lib
+import Test.HUnit
 
 main :: IO ()
 main = runTestTT tests >> return ()
 
-
 test31 :: Test
-test31 = let
-        matrix = fromLists
-            [ [Null, Null, Null]
-            , [Null, Null, Null]
-            , [Null, Null, Null]
-            ]
-    in
-        TestCase $ assertEqual "Not win size 3" (checkWin matrix) Null
+test31 =
+  let matrix =
+        fromLists
+          [ [Null, Null, Null],
+            [Null, Null, Null],
+            [Null, Null, Null]
+          ]
+   in TestCase $ assertEqual "Not win size 3" (checkWin matrix) Null
 
 test32 :: Test
-test32 = let
-        matrix = fromLists
-            [ [X, Null, Null]
-            , [Null, X, Null]
-            , [Null, Null, X]
-            ]
-    in
-        TestCase $ assertEqual "Win diag 3" (checkWin matrix) X
+test32 =
+  let matrix =
+        fromLists
+          [ [X, Null, Null],
+            [Null, X, Null],
+            [Null, Null, X]
+          ]
+   in TestCase $ assertEqual "Win diag 3" (checkWin matrix) X
 
 test33 :: Test
-test33 = let
-        matrix = fromLists
-            [ [Null, Null, X]
-            , [Null, X, Null]
-            , [X, Null, Null]
-            ]
-    in
-        TestCase $ assertEqual "Win another diag 3" (checkWin matrix) X
+test33 =
+  let matrix =
+        fromLists
+          [ [Null, Null, X],
+            [Null, X, Null],
+            [X, Null, Null]
+          ]
+   in TestCase $ assertEqual "Win another diag 3" (checkWin matrix) X
 
 test34 :: Test
-test34 = let
-        matrix = fromLists
-            [ [   X,    X,    X]
-            , [Null, Null, Null]
-            , [Null, Null, Null]
-            ]
-    in
-        TestCase $ assertEqual "Win horizontal size 3" (checkWin matrix) X
+test34 =
+  let matrix =
+        fromLists
+          [ [X, X, X],
+            [Null, Null, Null],
+            [Null, Null, Null]
+          ]
+   in TestCase $ assertEqual "Win horizontal size 3" (checkWin matrix) X
 
 test35 :: Test
-test35 = let
-        matrix = fromLists
-            [ [O,    X,    X]
-            , [O, Null, Null]
-            , [O, Null, Null]
-            ]
-    in
-        TestCase $ assertEqual "Win vertical size 3" (checkWin matrix) O
+test35 =
+  let matrix =
+        fromLists
+          [ [O, X, X],
+            [O, Null, Null],
+            [O, Null, Null]
+          ]
+   in TestCase $ assertEqual "Win vertical size 3" (checkWin matrix) O
 
 test36 :: Test
-test36 = let
-        matrix = fromLists
-            [ [X, X, O]
-            , [O, O, X]
-            , [X, X, O]
-            ]
-    in
-        TestCase $ assertEqual "Not win size 3" Null (checkWin matrix)
+test36 =
+  let matrix =
+        fromLists
+          [ [X, X, O],
+            [O, O, X],
+            [X, X, O]
+          ]
+   in TestCase $ assertEqual "Not win size 3" Null (checkWin matrix)
 
 testDecision1 :: Test
-testDecision1 = let
-        matrixStart = fromLists
-            [ [   X,    O,    O]
-            , [Null, Null,    X]
-            , [   X,    O,    O]
-            ]
-        matrixEnd = nextStepMatrix X matrixStart
-    in
-        TestCase $ assertEqual "X must win" X (checkWin matrixEnd)
+testDecision1 =
+  let matrixStart =
+        fromLists
+          [ [X, O, O],
+            [Null, Null, X],
+            [X, O, O]
+          ]
+      matrixEnd = nextStepMatrix X matrixStart
+   in TestCase $ assertEqual "X must win" X (checkWin matrixEnd)
+
+testDecision2 =
+  let matrixStart =
+        fromLists
+          [ [X, O, O, Null],
+            [X, X, X, O],
+            [O, O, X, Null],
+            [X, O, O, Null]
+          ]
+      matrixEnd = nextStepMatrix X matrixStart
+   in TestCase $ assertEqual "X must win" X (checkWin matrixEnd)
 
 tests :: Test
-tests = TestList [ TestLabel "check win" test31
-                 , TestLabel "check win" test32
-                 , TestLabel "check win" test33
-                 , TestLabel "check win" test34
-                 , TestLabel "check win" test35
-                 , TestLabel "check win" test36
-                 , TestLabel "check makeBestDecision" testDecision1
-                 ]
+tests =
+  TestList
+    [ TestLabel "check win" test31,
+      TestLabel "check win" test32,
+      TestLabel "check win" test33,
+      TestLabel "check win" test34,
+      TestLabel "check win" test35,
+      TestLabel "check win" test36,
+      TestLabel "check makeBestDecision" testDecision1,
+      TestLabel "check makeBestDecision 4" testDecision2
+    ]
